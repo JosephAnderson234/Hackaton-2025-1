ed245705cb93decab89abab5b5298870ee9f2d3a
JosephAnderson234 <joseph.cose@utec.edu.pe>
2025-05-07 17:44:08 -0500

    first

---
+++ src/main/java/com/example/hacktanton1/Hacktanton1Application.java
@@ -0,0 +1,13 @@
+package com.example.hacktanton1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hacktanton1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hacktanton1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hacktanton1/Hacktanton1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hacktanton1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hacktanton1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
